68d0b73cf71eeed661cccd8fafc0a8f239785b40
Antonio Vivace <dev@avivace.com>
2018-01-16 22:37:29 +0100

    Update build.sh

--- alacritty/build.sh
+++ alacritty/build.sh
@@ -1,8 +1,17 @@
-git clone https://github.com/jwilm/alacritty.git
-cargo install cargo-deb
-cd alacritty
-rustup override set stable
-rustup update stable
-sudo apt-get install cmake libfreetype6-dev libfontconfig1-dev xclip
+#!/bin/bash                                                                     
+# FIRST RUN
+# git clone https://github.com/jwilm/alacritty.git
+# cargo install cargo-deb
+# rustup override set stable
+# rustup update stable
+# sudo apt-get install cmake libfreetype6-dev libfontconfig1-dev xclip
+
+cd alacritty                                                                    
+git pull                                                                        
 cargo build --release
-cargo deb
+cargo deb                                                                       
+sudo aptly repo remove repo1 alacritty                                          
+sudo rm /root/.aptly/public/pool/main/a/alacritty/alacritty_0.1.0_amd64.deb     
+sudo aptly repo add repo1 target/debian/*.deb                                   
+sudo aptly publish update -batch unstable                                       
+sudo cp /root/.aptly/public/ /var/www/deb/ -r
