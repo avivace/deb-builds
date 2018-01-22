db4ca60c571ce35cf151d6c059dc9d916dd0fca6
Antonio Vivace <puma95.95@gmail.com>
2018-01-22 12:39:13 +0100

    use global update repo script

--- alacritty/build.sh
+++ alacritty/build.sh
@@ -6,12 +6,11 @@
 # rustup update stable
 # sudo apt-get install cmake libfreetype6-dev libfontconfig1-dev xclip
 
-cd alacritty                                                                    
-git pull                                                                        
+cd alacritty
+git pull
 cargo build --release
-cargo deb                                                                       
-sudo aptly repo remove repo1 alacritty                                          
-sudo rm /root/.aptly/public/pool/main/a/alacritty/alacritty_0.1.0_amd64.deb     
-sudo aptly repo add repo1 target/debian/*.deb                                   
-sudo aptly publish update -batch unstable                                       
-sudo cp /root/.aptly/public/ /var/www/deb/ -r
+cargo deb
+sudo aptly repo remove repo1 alacritty
+sudo rm /root/.aptly/public/pool/main/a/alacritty/alacritty_0.1.0_amd64.deb
+sudo aptly repo add repo1 target/debian/*.deb
+bash ../updateRepo.sh
